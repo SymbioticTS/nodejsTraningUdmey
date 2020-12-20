453cff8dd0ed54618325e4b5a58894fd051412da
Lalit <lalitnayyar@gmail.com>
2020-12-20 12:05:42 +0400

    using mongodb to store sessions
    using mongodb to store sessions

--- app.js
+++ app.js
@@ -4,11 +4,17 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
 
 const errorController = require('./controllers/error');
 const User = require('./models/user');
 
 const app = express();
+const MongoDBURI = 'mongodb+srv://lalitnayyar:Sharda180673@cluster0.ow0zp.mongodb.net/shop';
+const store = new MongoDBStore({
+  uri: MongoDBURI,
+  collection: 'sessions'
+});
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -19,7 +25,7 @@ const authRoutes = require('./routes/auth');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(session({secret:'my secret', resave: false, saveUninitialized:false}));
+app.use(session({secret:'my secret', resave: false, saveUninitialized:false,store: store}));
 
 app.use((req, res, next) => {
   User.findById('5bab316ce0a7c75f783cb8a8')
--- controllers/auth.js
+++ controllers/auth.js
@@ -2,7 +2,7 @@ exports.getLogin = (req, res, next) => {
   //console.log(req.get("Cookie").split(";")[1].trim().split("=")[1]);
   //isLoggedIn = req.get("Cookie").split(";")[1].trim().split("=")[1] === 'true';
   // console.log(req.get('Cookie'));
-  console.log(req.session);
+  console.log(req.session.isLoggedIn);
   res.render("auth/login", {
     path: "/login",
     pageTitle: "Login",
