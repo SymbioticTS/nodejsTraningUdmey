7a3335fb492a5378665afe7f6d7f24917fbbe44f
Lalit <lalitnayyar@gmail.com>
2020-12-20 11:05:48 +0400

    session cookies
    session cookies

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,23 +1,19 @@
 exports.getLogin = (req, res, next) => {
-  console.log(req.get("Cookie").split(";")[1].trim().split("=")[1]);
-  isLoggedIn = req.get("Cookie").split(";")[1].trim().split("=")[1] === 'true';
+  //console.log(req.get("Cookie").split(";")[1].trim().split("=")[1]);
+  //isLoggedIn = req.get("Cookie").split(";")[1].trim().split("=")[1] === 'true';
   // console.log(req.get('Cookie'));
+  console.log(req.session);
   res.render("auth/login", {
     path: "/login",
     pageTitle: "Login",
-    isAuthenticated: isLoggedIn,
+    isAuthenticated: req.session.isLoggedIn,
   });
 };
 
 exports.postLogin = (req, res, next) => {
   //res.setHeader('Set-Cookie',{loggedin:true,test:true})  ;
-  //cookie setting
-  var cookieString = "";
-  if (cookieString) {
-    cookieString += ";";
-  }
-  cookieString +=  `loggedin=true;test=false`;
-  // `loggedin=true;Max-Age=10;Secure;test=false`;
-  res.setHeader("Set-Cookie", cookieString);
+  //cookie settings
+  req.session.isLoggedIn=true;
+  req.session.testCase= '123';
   res.redirect("/");
 };
